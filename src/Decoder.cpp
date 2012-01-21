9468e15b5207c58a53aa3a632f1cd10def5253ea
Rick Taylor <rick@tss07.home.tropicalstormsoftware.com>
2012-01-21 17:50:11 +0000

    Add copyright statement
    Signed-off-by: Rick Taylor <rick@tss07.home.tropicalstormsoftware.com>

--- src/Decoder.cpp
+++ src/Decoder.cpp
@@ -1,9 +1,23 @@
-/*
- * Decoder.cpp
- *
- *  Created on: 19 Jan 2012
- *      Author: taylorr
- */
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2012 Rick Taylor
+//
+// This file is part of OOXML, the Omega Online XML library.
+//
+// OOXML is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OOXML is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OOXML.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////////
 
 #include "Decoder.h"
 
